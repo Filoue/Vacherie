74b71b88d4db0f28f9eae85fb33752c252ac154e
GOD <thediscordingtales@gmail.com>
2024-07-22 18:12:55 +0200

    StaminaBar for cows + TreeSwaying
    Vraiment primaire

---
+++ Assets/Scripts/Stamina.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cow : MonoBehaviour
+{
+    public Slider staminaBar;
+    public float maxStamina = 100f;
+    public float currentStamina;
+    public float staminaDepletionRate = 10f; // Stamina depleted per second
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        if (staminaBar != null)
+        {
+            staminaBar.maxValue = maxStamina;
+            staminaBar.value = currentStamina;
+        }
+    }
+
+    void Update()
+    {
+        if (currentStamina > 0)
+        {
+            currentStamina -= staminaDepletionRate * Time.deltaTime;
+            if (staminaBar != null)
+            {
+                staminaBar.value = currentStamina;
+            }
+        }
+    }
+}
