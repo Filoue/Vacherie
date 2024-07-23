cf064f3da2f6085a623370b5936882d9e5377930
Filoue <tonarypetitfiloue@gmail.com>
2024-07-23 17:18:26 +0200

    coucou

---
+++ Assets/Scripts/Proceduraltree.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Proceduraltree : MonoBehaviour
+{
+    public GameObject treePrefab; // Le prefab de l'arbre à placer
+    public int numberOfTrees = 100; // Le nombre d'arbres à placer
+    public float planeSize = 10f; // La taille du plane
+
+    void Start()
+    {
+        PlaceTrees();
+    }
+
+    void PlaceTrees()
+    {
+        for (int i = 0; i < numberOfTrees; i++)
+        {
+            // Génère des positions aléatoires sur le plane
+            float xPosition = Random.Range(-planeSize / 2, planeSize / 2);
+            float zPosition = Random.Range(-planeSize / 2, planeSize / 2);
+
+            // Calcule la position de l'arbre
+            Vector3 treePosition = new Vector3(xPosition, 0, zPosition);
+
+            // Instantie l'arbre à la position calculée
+            Instantiate(treePrefab, treePosition, Quaternion.identity);
+
+
+        }
+    }
+}
