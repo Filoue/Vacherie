cdb1db85d5ddef8e0ec0f9ba04246b37ded91802
GOD <thediscordingtales@gmail.com>
2024-07-26 21:30:05 +0200

    Added Grass and Growing Path to HuguesTest2

---
+++ Assets/Shader/CutOff/EdgeGrower.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EdgeGrower : MonoBehaviour
+{
+    public float animationSpeed = 0.2f;
+    private SpriteRenderer spriteRenderer;
+    private float currentTime;
+    private string shaderProperty = "_Edge";
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentTime = 0f;
+        spriteRenderer.material.SetFloat(shaderProperty, 0.07f);
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        float edgeValue = Mathf.Clamp01(0.05f + currentTime * animationSpeed);
+        spriteRenderer.material.SetFloat(shaderProperty, edgeValue);
+    }
+}
