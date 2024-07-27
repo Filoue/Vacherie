451e409512da9c9f605c8a4ba72d82e37fa589f9
GOD <thediscordingtales@gmail.com>
2024-07-27 10:32:02 +0200

    CowMilk script update

--- Assets/Scripts/CowMilked.cs
+++ Assets/Scripts/CowMilked.cs
@@ -6,36 +6,12 @@ public class CowMilked : MonoBehaviour
 {
     public EntitiesManager entitiesManager;
     public Slider staminaSlider;
-    public float milkValueMultiplier = 10f; // Configurable value for the score
-
-    // Variables for saving the number of cows and spawning configuration
-    private int savedNumberOfCows;
-    public Vector3 spawnPosition;
-    public float spacing = 2f; // Configurable spacing between cows
-
-
-    public GameObject cowPrefab;
+    public float milkValueMultiplier = 16.67f;
     public MilkFill milkFill;
 
     void Start()
     {
-        if (entitiesManager == null)
-        {
-            entitiesManager = FindObjectOfType<EntitiesManager>();
-        }
-
-        if (staminaSlider == null)
-        {
-            staminaSlider = GetComponentInChildren<Slider>();
-        }
-
-        if (milkFill == null)
-        {
-            milkFill = FindObjectOfType<MilkFill>();
-        }
-
-        UpdateStaminaSlider();
-        SaveNumberOfCows();
+        Initialize();
     }
 
     public void Initialize()
@@ -56,7 +32,6 @@ public class CowMilked : MonoBehaviour
         }
 
         UpdateStaminaSlider();
-        SaveNumberOfCows();
     }
 
     public void UpdateStaminaSlider()
@@ -67,26 +42,6 @@ public class CowMilked : MonoBehaviour
             float newSliderValue = numberOfCows * milkValueMultiplier;
             staminaSlider.value = newSliderValue;
             milkFill.SetTargetValue(newSliderValue); // Set target value for MilkFill
-        }
-    }
-
-    public void SaveNumberOfCows()
-    {
-        if (entitiesManager != null)
-        {
-            savedNumberOfCows = entitiesManager.cows.Count;
-        }
-    }
-
-    public void SpawnCows()
-    {
-        if (cowPrefab != null)
-        {
-            for (int i = 0; i < savedNumberOfCows; i++)
-            {
-                Vector3 position = spawnPosition + new Vector3(i * spacing, 0, 0);
-                Instantiate(cowPrefab, position, Quaternion.identity);
-            }
         }
     }
 }
